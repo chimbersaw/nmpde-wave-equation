3332d5d0ced415f1aeba4caadbcdd963f6334a22
Maksim Sukhodolskii <maksukh@inbox.ru>
2026-02-16 20:46:23 +0100

    Fix namespaces

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,5 @@
 #include "WaveEquation.hpp"
 
-int main(int argc, char *argv[])
-{
-  // Inizializzazione MPI 
-  Utilities::MPI::MPI_InitFinalize mpi_init(argc, argv);
-  
-  // Dimensione spaziale 2D per l'equazione delle onde
-  const unsigned int dim = 2;
-
-  // Parametri della simulazione:
-  // 1. Percorso del file mesh (.msh)
-  // 2. Grado polinomiale degli elementi finiti (1 = lineari)
-  // 3. Tempo finale della simulazione (T)
-  // 4. Passo temporale (delta_t)
-  WaveEquation<dim> wave_problem("../mesh/mesh-square.msh", 1, 2.0, 0.01);
-
-  // Lancio della simulazione
-  wave_problem.run();
-
-  return 0;
+int main(int argc, char *argv[]) {
+    return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
